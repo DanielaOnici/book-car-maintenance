d78eca16c4d6a28510a3baa0dcb9c4e3e4b778a7
Daniela Onici <danielaonici@gmail.com>
2022-06-11 15:52:19 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace book_car_maintenance
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbCustomerName = new System.Windows.Forms.TextBox();
+            this.txtbAddress = new System.Windows.Forms.TextBox();
+            this.txtbCity = new System.Windows.Forms.TextBox();
+            this.txtbPostalCode = new System.Windows.Forms.TextBox();
+            this.txtbEmail = new System.Windows.Forms.TextBox();
+            this.txtbCellPhone = new System.Windows.Forms.TextBox();
+            this.txtbHomePhone = new System.Windows.Forms.TextBox();
+            this.txtbProvince = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblErrorMessageOne = new System.Windows.Forms.Label();
+            this.lblErrorMessageTwo = new System.Windows.Forms.Label();
+            this.lblErrorMessageThree = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblHomePhone = new System.Windows.Forms.Label();
+            this.lblCellPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbCustomerName
+            // 
+            this.txtbCustomerName.Location = new System.Drawing.Point(139, 22);
+            this.txtbCustomerName.Name = "txtbCustomerName";
+            this.txtbCustomerName.Size = new System.Drawing.Size(195, 23);
+            this.txtbCustomerName.TabIndex = 0;
+            // 
+            // txtbAddress
+            // 
+            this.txtbAddress.Location = new System.Drawing.Point(139, 67);
+            this.txtbAddress.Name = "txtbAddress";
+            this.txtbAddress.Size = new System.Drawing.Size(195, 23);
+            this.txtbAddress.TabIndex = 1;
+            // 
+            // txtbCity
+            // 
+            this.txtbCity.Location = new System.Drawing.Point(139, 115);
+            this.txtbCity.Name = "txtbCity";
+            this.txtbCity.Size = new System.Drawing.Size(195, 23);
+            this.txtbCity.TabIndex = 2;
+            // 
+            // txtbPostalCode
+            // 
+            this.txtbPostalCode.Location = new System.Drawing.Point(139, 165);
+            this.txtbPostalCode.Name = "txtbPostalCode";
+            this.txtbPostalCode.Size = new System.Drawing.Size(195, 23);
+            this.txtbPostalCode.TabIndex = 3;
+            // 
+            // txtbEmail
+            // 
+            this.txtbEmail.Location = new System.Drawing.Point(139, 356);
+            this.txtbEmail.Name = "txtbEmail";
+            this.txtbEmail.Size = new System.Drawing.Size(195, 23);
+            this.txtbEmail.TabIndex = 7;
+            // 
+            // txtbCellPhone
+            // 
+            this.txtbCellPhone.Location = new System.Drawing.Point(139, 306);
+            this.txtbCellPhone.Name = "txtbCellPhone";
+            this.txtbCellPhone.Size = new System.Drawing.Size(195, 23);
+            this.txtbCellPhone.TabIndex = 6;
+            // 
+            // txtbHomePhone
+            // 
+            this.txtbHomePhone.Location = new System.Drawing.Point(139, 258);
+            this.txtbHomePhone.Name = "txtbHomePhone";
+            this.txtbHomePhone.Size = new System.Drawing.Size(195, 23);
+            this.txtbHomePhone.TabIndex = 5;
+            // 
+            // txtbProvince
+            // 
+            this.txtbProvince.Location = new System.Drawing.Point(139, 213);
+            this.txtbProvince.Name = "txtbProvince";
+            this.txtbProvince.Size = new System.Drawing.Size(195, 23);
+            this.txtbProvince.TabIndex = 4;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(567, 115);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(195, 23);
+            this.textBox9.TabIndex = 10;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(567, 67);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(195, 23);
+            this.textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(567, 22);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(195, 23);
+            this.textBox11.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(401, 165);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(361, 164);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Book Appointment";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(542, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 21);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 21);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(694, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 21);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Pre-fill";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // lblErrorMessageOne
+            // 
+            this.lblErrorMessageOne.AutoSize = true;
+            this.lblErrorMessageOne.Location = new System.Drawing.Point(36, 397);
+            this.lblErrorMessageOne.Name = "lblErrorMessageOne";
+            this.lblErrorMessageOne.Size = new System.Drawing.Size(0, 15);
+            this.lblErrorMessageOne.TabIndex = 16;
+            // 
+            // lblErrorMessageTwo
+            // 
+            this.lblErrorMessageTwo.AutoSize = true;
+            this.lblErrorMessageTwo.Location = new System.Drawing.Point(36, 426);
+            this.lblErrorMessageTwo.Name = "lblErrorMessageTwo";
+            this.lblErrorMessageTwo.Size = new System.Drawing.Size(0, 15);
+            this.lblErrorMessageTwo.TabIndex = 17;
+            // 
+            // lblErrorMessageThree
+            // 
+            this.lblErrorMessageThree.AutoSize = true;
+            this.lblErrorMessageThree.Location = new System.Drawing.Point(36, 454);
+            this.lblErrorMessageThree.Name = "lblErrorMessageThree";
+            this.lblErrorMessageThree.Size = new System.Drawing.Size(0, 15);
+            this.lblErrorMessageThree.TabIndex = 18;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(23, 25);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(97, 15);
+            this.lblCustomerName.TabIndex = 19;
+            this.lblCustomerName.Text = "Customer Name:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(68, 70);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(52, 15);
+            this.lblAddress.TabIndex = 20;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(89, 118);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(31, 15);
+            this.lblCity.TabIndex = 21;
+            this.lblCity.Text = "City:";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Location = new System.Drawing.Point(47, 168);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(73, 15);
+            this.lblPostalCode.TabIndex = 22;
+            this.lblPostalCode.Text = "Postal Code:";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Location = new System.Drawing.Point(64, 216);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(56, 15);
+            this.lblProvince.TabIndex = 23;
+            this.lblProvince.Text = "Province:";
+            // 
+            // lblHomePhone
+            // 
+            this.lblHomePhone.AutoSize = true;
+            this.lblHomePhone.Location = new System.Drawing.Point(40, 261);
+            this.lblHomePhone.Name = "lblHomePhone";
+            this.lblHomePhone.Size = new System.Drawing.Size(80, 15);
+            this.lblHomePhone.TabIndex = 24;
+            this.lblHomePhone.Text = "Home Phone:";
+            // 
+            // lblCellPhone
+            // 
+            this.lblCellPhone.AutoSize = true;
+            this.lblCellPhone.Location = new System.Drawing.Point(53, 309);
+            this.lblCellPhone.Name = "lblCellPhone";
+            this.lblCellPhone.Size = new System.Drawing.Size(67, 15);
+            this.lblCellPhone.TabIndex = 25;
+            this.lblCellPhone.Text = "Cell Phone:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(81, 358);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(39, 15);
+            this.lblEmail.TabIndex = 26;
+            this.lblEmail.Text = "Email:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblCellPhone);
+            this.Controls.Add(this.lblHomePhone);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.lblErrorMessageThree);
+            this.Controls.Add(this.lblErrorMessageTwo);
+            this.Controls.Add(this.lblErrorMessageOne);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.txtbEmail);
+            this.Controls.Add(this.txtbCellPhone);
+            this.Controls.Add(this.txtbHomePhone);
+            this.Controls.Add(this.txtbProvince);
+            this.Controls.Add(this.txtbPostalCode);
+            this.Controls.Add(this.txtbCity);
+            this.Controls.Add(this.txtbAddress);
+            this.Controls.Add(this.txtbCustomerName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbCustomerName;
+        private System.Windows.Forms.TextBox txtbAddress;
+        private System.Windows.Forms.TextBox txtbCity;
+        private System.Windows.Forms.TextBox txtbPostalCode;
+        private System.Windows.Forms.TextBox txtbEmail;
+        private System.Windows.Forms.TextBox txtbCellPhone;
+        private System.Windows.Forms.TextBox txtbHomePhone;
+        private System.Windows.Forms.TextBox txtbProvince;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblErrorMessageOne;
+        private System.Windows.Forms.Label lblErrorMessageTwo;
+        private System.Windows.Forms.Label lblErrorMessageThree;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Label lblHomePhone;
+        private System.Windows.Forms.Label lblCellPhone;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
+
